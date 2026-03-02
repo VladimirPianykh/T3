1fac8ed84684c018108197ef5ea7a30269d21b32
icedragon2008 <ice_dragon_black@mail.ru>
2026-03-02 19:15:06 +0300

    S1

--- ntoproject/src/main/java/com/ntoproject/Main.java
+++ ntoproject/src/main/java/com/ntoproject/Main.java
@@ -6,7 +6,8 @@ import com.bpa4j.util.codegen.ProjectGraph;
 
 public class Main {
     public static void main(String[] args) {
-        new ProjectGraph(new File("C:\\Users\\user\\Desktop\\IT\\Java\\1C\\NTO training\\team\\T3\\ntoproject\\src\\main\\java")).show();
+        // new ProjectGraph(new File("C:\\Users\\user\\Desktop\\IT\\Java\\1C\\NTO training\\team\\T3\\ntoproject\\src\\main\\java")).show();
+        new ProjectGraph(new File("C:\\Users\\ice_d\\Desktop\\Моё\\Програмирование\\NTO training\\T3\\ntoproject\\src\\main\\java")).show();
         System.out.println("Hello world!");
     }
 }

8ce4f14ce2f2b58a220ef8fc501503ad814741bc
VladimirPianykh <vovapianykh21@gmail.com>
2026-03-02 19:58:28 +0400

    Added projectGraph invokaction

--- ntoproject/src/main/java/com/ntoproject/Main.java
+++ ntoproject/src/main/java/com/ntoproject/Main.java
@@ -1,7 +1,12 @@
 package com.ntoproject;
 
+import java.io.File;
+
+import com.bpa4j.util.codegen.ProjectGraph;
+
 public class Main {
     public static void main(String[] args) {
+        new ProjectGraph(new File("C:\\Users\\user\\Desktop\\IT\\Java\\1C\\NTO training\\team\\T3\\ntoproject\\src\\main\\java")).show();
         System.out.println("Hello world!");
     }
 }

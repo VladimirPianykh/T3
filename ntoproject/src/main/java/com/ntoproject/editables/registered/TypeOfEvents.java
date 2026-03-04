9743443c84b300dee346dbcb19b95f677ccad330
icedragon2008 <ice_dragon_black@mail.ru>
2026-03-04 21:04:25 +0300

    TimesToFast

---
+++ ntoproject/src/main/java/com/ntoproject/editables/registered/TypeOfEvents.java
@@ -0,0 +1,10 @@
+package com.ntoproject.editables.registered;
+
+import com.bpa4j.core.Data.Editable;
+
+public class TypeOfEvents extends Editable{
+    
+    public TypeOfEvents(){
+        super("Новый тип мероприятия");
+    }
+}

cd93fd89c3ce3987ce09d1691d253b0d2e621671
VladimirPianykh <vovapianykh21@gmail.com>
2026-03-04 21:24:22 +0400

    Added location to spaces. Spent too much time

--- ntoproject/src/main/java/com/ntoproject/Main.java
+++ ntoproject/src/main/java/com/ntoproject/Main.java
@@ -57,7 +57,7 @@ public class Main {
         private AppPermission(){Registrator.register(this);}
     }
 
-    public static void main(String[] args) {
+    public static void main(String[]args){
         // new ProjectGraph(new File("C:\\Users\\user\\Desktop\\IT\\Java\\1C\\NTO training\\team\\T3\\ntoproject\\src\\main\\java")).show();
         // new ProjectGraph(new File("C:\\Users\\ice_d\\Desktop\\Моё\\Програмирование\\NTO training\\T3\\ntoproject\\src\\main\\java")).show();
         // Navigator.init();
